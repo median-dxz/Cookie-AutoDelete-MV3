d2538a0d9969f9ca358879097044cdafe73ea698
median-dxz <median.dxz@gmail.com>
2025-07-18 20:01:36 +0800

    chore: migrate ESLint configuration to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier/flat';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
+
+export default tsEslint.config(
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+        webextensions: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+  },
+  eslint.configs.recommended,
+  tsEslint.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'warn',
+    },
+  },
+  prettier,
+);
