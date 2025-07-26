bd77a16afd5f84a2229ce6a3180c02e158e1ab3d
median-dxz <median.dxz@gmail.com>
2025-07-27 00:27:39 +0800

    feat: support different browser environments in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import manifestJson from './manifest.json';
 import packageJson from './package.json';
 import * as DIR from './tools/directories';
@@ -9,53 +9,67 @@ type FixEmailFields<T> = {
   [TKey in keyof T]: TKey extends 'author' ? { email: string } : T[TKey];
 };
 
-const manifest = {
-  ...manifestJson,
-  version: packageJson.version,
-} as unknown as FixEmailFields<typeof manifestJson>;
+type Browser = 'firefox' | 'chrome';
 
-export default defineConfig({
-  publicDir: DIR.EXT,
-  build: {
-    outDir: DIR.DIST,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (
-            id.includes('react') ||
-            id.includes('redux') ||
-            id.includes('fontawsome')
-          ) {
-            return 'react';
-          }
-          if (id.includes('webextension-polyfill')) {
-            return 'webextension-polyfill';
-          }
+export default defineConfig(() => {
+  const browser: Browser = (process.env.BROWSER as Browser) ?? 'chrome';
+
+  const manifest = {
+    ...manifestJson,
+    version: packageJson.version,
+  } as unknown as FixEmailFields<typeof manifestJson>;
+
+  if (browser === 'chrome') {
+    manifest.permissions.splice(
+      manifest.permissions.indexOf('contextualIdentities'),
+      1,
+    );
+    delete manifest.browser_specific_settings;
+  }
+
+  return {
+    publicDir: DIR.EXT,
+    build: {
+      outDir: DIR.DIST,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (
+              id.includes('react') ||
+              id.includes('redux') ||
+              id.includes('fontawsome')
+            ) {
+              return 'react';
+            }
+            if (id.includes('webextension-polyfill')) {
+              return 'webextension-polyfill';
+            }
+          },
         },
       },
     },
-  },
-  plugins: [react(), crx({ manifest })],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: [
-          'import',
-          'mixed-decls',
-          'color-functions',
-          'global-builtin',
-        ],
+    plugins: [react(), crx({ manifest })],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
       },
     },
-  },
-  server: {
-    cors: {
-      origin: [/chrome-extension:\/\//],
+    server: {
+      cors: {
+        origin: [/chrome-extension:\/\//],
+      },
+      strictPort: true,
+      port: 5173,
+      hmr: {
+        clientPort: 5173,
+      },
     },
-    strictPort: true,
-    port: 5173,
-    hmr: {
-      clientPort: 5173,
-    },
-  },
+  } satisfies UserConfig;
 });
