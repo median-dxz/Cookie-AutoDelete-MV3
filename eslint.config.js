d22770ced82034c3926174a4c142311a7cca2e64
median-dxz <median.dxz@gmail.com>
2025-11-11 23:13:50 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,11 @@ import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier/flat';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import { globalIgnores } from 'eslint/config';
+import { globalIgnores, defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tsEslint from 'typescript-eslint';
 
-export default tsEslint.config(
+export default defineConfig(
   globalIgnores(['./coverage/']),
   {
     languageOptions: {
@@ -29,13 +29,14 @@ export default tsEslint.config(
       },
     },
   },
+
   {
     files: ['src/**/*.{ts,tsx}'],
     extends: [
       eslint.configs.recommended,
       tsEslint.configs.strict,
       react.configs.flat.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat.recommended,
       prettier,
     ],
 
